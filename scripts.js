225c2169319e468741833f65cdfc71d8531bab78
Parsanov <romaparsanov@gmail.com>
2023-12-05 19:20:52 +0200

    add file

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+let car = {
+    brand: 'BMW',
+    model: '340M',
+    year: 2022
+};
+
+let keys = Object.keys(car);
+let values = Object.values(car);
+let entries = Object.entries(car);
+
+
+console.log('Keys:', keys);
+console.log('Values:', values);
+console.log('Entries:', entries);
